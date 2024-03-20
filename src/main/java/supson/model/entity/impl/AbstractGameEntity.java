c7ce58c81c271351528b21e10165222cff9796a8
Matteo <matteobagnolini2003@gmail.com>
2024-03-20 09:03:49 +0100

    add abstract implementation of game entity

---
+++ src/main/java/supson/model/entity/impl/AbstractGameEntity.java
@@ -0,0 +1,31 @@
+package supson.model.entity.impl;
+
+import supson.common.api.Pos2d;
+import supson.model.entity.api.GameEntity;
+import supson.model.hitbox.api.Hitbox;
+
+/**
+ * This abstract class, which implements the interface GameEntity, models a generic game entity.
+ * This class is used as a template to create other more specific classes.
+ */
+public abstract class GameEntityImpl implements GameEntity {
+
+    private Pos2d position;
+    private Hitbox hitbox;
+
+    @Override
+    public final Pos2d getPosition() {
+        return this.position;
+    }
+
+    @Override
+    public final void setPosition(final Pos2d pos) {
+        this.position = pos;
+    }
+
+    @Override
+    public final Hitbox getHitbox() {
+        return this.hitbox;
+    }
+
+}
