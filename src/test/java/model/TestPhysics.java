6d433df074586e012bfecbac0073adf47c9e5dfb
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-05-13 13:04:25 +0200

    add new physics test not implemented

---
+++ src/test/java/model/TestPhysics.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+
+
+class TestPhysics {
+
+
+/* 
+    private MoveableEntity entity = new AbstractMoveableEntity(null, 0, 0, null, 0, null) {
+
+        @Override
+        protected void updateVelocity() {
+            
+        }
+        
+    };*/
+
+    @BeforeEach
+    private void init() {
+        //this.physics = new PhysicsImpl(0, 0, 0, 0)
+    }
+
+}
