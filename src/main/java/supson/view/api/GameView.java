46024b13d7cad8c9e99869485f6137671005169c
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-05-13 14:47:02 +0200

    Recreation of the view interfaces and removal of the previous implementations.

--- src/main/java/supson/view/api/GameView.java
+++ src/main/java/supson/view/api/GameView.java
@@ -1,25 +1,18 @@
 package supson.view.api;
 
-import java.util.List;
-
-import supson.model.block.api.BlockEntity;
-import supson.model.entity.impl.Enemy;
-import supson.model.entity.impl.Player;
-
+/**
+ * The GameView interface represents the view component of the game.
+ * It provides methods for initializing and rendering the game.
+ */
 public interface GameView {
-
+    
     /**
-     * Renders the game panel and starts the game.
+     * Initializes the view.
      */
-    void renderStartGame();
-
+    void initView();
+    
     /**
-     * Renders the game by rendering the level, enemies, player, and HUD on the game panel.
-     * 
-     * @param blocks the list of block entities to render
-     * @param enemies the list of enemy entities to render
-     * @param player the player entity to render
+     * Renders the view.
      */
-    void renderGame(List<BlockEntity> blocks, List<Enemy> enemies, Player player);
-
+    void renderView();
 }
