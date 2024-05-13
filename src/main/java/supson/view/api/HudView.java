0e03decf7d8fcfed2e93b8dcf6abad3b78351318
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-05-13 15:47:45 +0200

    Removal of parameters from the view interfaces.

--- src/main/java/supson/view/api/HudView.java
+++ src/main/java/supson/view/api/HudView.java
@@ -1,12 +1,8 @@
 package supson.view.api;
 
-import supson.model.entity.impl.Player;
-
 public interface HudView {
     /**
     * Renders the HUD.
-    * 
-    * @param player the player object to render the HUD for
     */
-    void renderHud(Player player);
+    void renderHud();
 }
--- src/main/java/supson/view/api/WorldView.java
+++ src/main/java/supson/view/api/WorldView.java
@@ -1,9 +1,5 @@
 package supson.view.api;
 
-import java.util.List;
-
-import supson.model.entity.api.GameEntity;
-
 /**
  * The WorldView interface represents the view of the game world.
  * It provides methods to render the game entities in the world.
@@ -11,8 +7,6 @@ import supson.model.entity.api.GameEntity;
 public interface WorldView {
     /**
      * Renders the game entities in the world.
-     * 
-     * @param gameEntityList a list of game entities to be rendered
-     */
-    void renderWorld(List<GameEntity> gameEntityList);
+    */
+    void renderWorld();
 }
