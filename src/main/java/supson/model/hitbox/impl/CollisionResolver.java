cca777eb6b7dae513ab924f429441d5134b8a864
Matteo <matteobagnolini2003@gmail.com>
2024-05-23 17:08:18 +0200

    modify collision method, now using isInvulnerable

--- src/main/java/supson/model/hitbox/impl/CollisionResolver.java
+++ src/main/java/supson/model/hitbox/impl/CollisionResolver.java
@@ -85,7 +85,7 @@ public final class CollisionResolver {
      */
     public static List<Enemy> resolveEnemiesCollisions(final Player player, final List<Enemy> enemies) {
         final Hitbox playerHitbox = player.getHitbox();
-        if (player.isJumping()) {
+        if (player.isInvulnerable()) {
              return enemies.stream()
             .filter(k -> playerHitbox.isCollidingWith(k.getHitbox()))
             .collect(Collectors.toList());
