af2dfab7960f58c27887b4af4c190ff51d1525e1
Matteo <matteobagnolini2003@gmail.com>
2024-05-24 11:02:40 +0200

    add documentation

--- src/main/java/supson/model/entity/impl/player/PlayerState.java
+++ src/main/java/supson/model/entity/impl/player/PlayerState.java
@@ -2,5 +2,9 @@ package supson.model.entity.impl.player;
 
 import supson.common.api.Vect2d;
 
+/**
+ * This class model the player state. It is immutable, therefore a new instance 
+ * should be created every time the state changes.
+ */
 public record PlayerState(Vect2d vel,boolean right, boolean left, boolean jump,
         boolean onGround, boolean isJumping, boolean isInvulnerable) { }
