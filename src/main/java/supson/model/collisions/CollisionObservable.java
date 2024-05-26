a02e7e9e8f86c1d017ec2811ec105d129480eae1
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-05-26 12:33:11 +0200

    add documentation for collision observer pattern

--- src/main/java/supson/model/collisions/CollisionObservable.java
+++ src/main/java/supson/model/collisions/CollisionObservable.java
@@ -1,11 +1,30 @@
 package supson.model.collisions;
 
+/**
+ * This is an interface to implement an observer design pattern for collisions.
+ * It represents an observable, that can be observed.
+ * @see supson.model.collisions.CollisionObserver
+ */
 public interface CollisionObservable {
     
+    /**
+     * This method registers a new observer, which will be notified
+     * about collision events.
+     * @param observer the observer to be registered
+     */
     void register(CollisionObserver observer);
 
+    /**
+     * This method unregisters an observer that was previously registeres.
+     * The behavior is undefined if the observer is not registered.
+     * @param observer the observer to be unregistered
+     */
     void unregister(CollisionObserver observer);
 
+    /**
+     * This method notify all the registered observer of an event occured.
+     * @param event the event occured
+     */
     void notifyObservers(CollisionEvent event);
 
 }
--- src/main/java/supson/model/collisions/CollisionObserver.java
+++ src/main/java/supson/model/collisions/CollisionObserver.java
@@ -1,7 +1,17 @@
 package supson.model.collisions;
 
+/**
+ * This is an interface to implement an observer design pattern for collisions.
+ * It represents an observer, that can be notified when an event occurs.
+ * @see supson.model.collisions.CollisionObservable
+ */
 public interface CollisionObserver {
     
+    /**
+     * This method perform some actions based on the event occured.
+     * It should be called by {@link supson.model.collisions.CollisionObservable#notifyObservers(CollisionEvent)}.
+     * @param event the event occured
+     */
     void onNotify(CollisionEvent event);
 
 }
