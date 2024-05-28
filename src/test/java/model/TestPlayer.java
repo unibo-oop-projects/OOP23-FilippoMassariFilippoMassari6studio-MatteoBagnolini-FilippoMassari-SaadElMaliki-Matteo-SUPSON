37d41c16124865c2db6bb380086a12ac07d4e328
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-05-28 18:01:41 +0200

    delete unnecessary import from player test

--- src/test/java/model/TestPlayer.java
+++ src/test/java/model/TestPlayer.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import supson.common.impl.Vect2dImpl;
 import supson.model.entity.player.Player;
