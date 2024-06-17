127ada82981efdc9de5416f6a1816a7d268e8110
Saad El Maliki <saad.elmaliki@studio.unibo.it>
2024-06-17 15:27:14 +0200

    removed BlockEntity references

--- src/main/java/supson/model/block/api/Finishline.java
+++ src/main/java/supson/model/block/api/Finishline.java
@@ -1,7 +1,8 @@
 package supson.model.block.api;
 
+import supson.model.entity.api.GameEntity;
 import supson.model.world.api.World;
 
-public interface Finishline extends BlockEntity {
+public interface Finishline extends GameEntity {
     void endGame(World world);
 }
