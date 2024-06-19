f31d25d3f21cc9308ad90626c56b10d95e899028
Saad El Maliki <saad.elmaliki@studio.unibo.it>
2024-06-19 19:28:13 +0200

    checkstyle fixes

--- src/main/java/supson/model/block/api/Finishline.java
+++ src/main/java/supson/model/block/api/Finishline.java
@@ -3,6 +3,16 @@ package supson.model.block.api;
 import supson.model.entity.api.GameEntity;
 import supson.model.world.api.World;
 
+/**
+ * The FinishLine interface represents a block entity that signals the reaching of the current level ending.
+ * It extends the GameEntity interface.
+ */
 public interface Finishline extends GameEntity {
+
+    /**
+     * Triggers the end of the current game when the player reaches the finishline.
+     * 
+     * @param world the game world which has to end
+     */
     void endGame(World world);
 }
--- src/main/java/supson/model/block/impl/FinishlineImpl.java
+++ src/main/java/supson/model/block/impl/FinishlineImpl.java
@@ -7,6 +7,10 @@ import supson.model.block.api.Finishline;
 import supson.model.entity.impl.AbstractGameEntity;
 import supson.model.world.api.World;
 
+/**
+ * Represents a finishline in the game.
+ * This finishline triggers the end of the current game world.
+ */
 public class FinishlineImpl extends AbstractGameEntity implements Finishline {
 
     private static final int HEIGHT = 2;
@@ -14,16 +18,19 @@ public class FinishlineImpl extends AbstractGameEntity implements Finishline {
 
     private static final GameEntityType TYPE = GameEntityType.FINISHLINE;
 
-
-    public FinishlineImpl(Pos2d pos) {
+    /**
+     * Construcs a new FinishlineImpl object with the specified position.
+     * 
+     * @param pos the position of the finishline
+     */
+    public FinishlineImpl(final Pos2d pos) {
         super(pos, HEIGHT, WIDTH, TYPE);
     }
 
     @Override
-    public void endGame(World world) {
+    public void endGame(final World world) {
         world.setGameOver(true);
         System.out.println("GAME OVER");
     }
-
 
 }
