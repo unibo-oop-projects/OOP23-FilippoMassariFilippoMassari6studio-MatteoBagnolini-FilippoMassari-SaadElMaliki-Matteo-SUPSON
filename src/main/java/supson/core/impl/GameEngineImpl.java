be7e5fad88087995ac09dba6b8dbd2f01b0888cb
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-06-25 12:21:22 +0200

    refactor controller (now in Engine)

--- src/main/java/supson/core/impl/GameEngineImpl.java
+++ src/main/java/supson/core/impl/GameEngineImpl.java
@@ -1,34 +1,68 @@
 package supson.core.impl;
 
 import supson.core.api.GameEngine;
-import supson.controller.api.GameController;
-import supson.controller.impl.GameControllerImpl;
+import supson.model.world.api.World;
+import supson.model.world.impl.WorldImpl;
+import supson.view.api.GameView;
+import supson.view.impl.GameViewImpl;
+import supson.view.impl.InputManager;
+
+import javax.swing.JFrame;
 
 /**
  * This class represents the main engine of the game.
  */
 public final class GameEngineImpl implements GameEngine {
 
+    private static final int WIDTH = 948;
+    private static final int HEIGHT = 720;
+
+    private static final String WORLD_FILE_PATH = "/level/level_1.txt";
+
     private static final long REFRESH_RATE = 20;
 
-    private final GameController controller;
+    private GameState state;
+
+    private final World model;
+    private final GameView view;
+    private final InputManager input;
+    private final JFrame mainFrame;
 
     /**
      * GameEngine constructor.
      */
     public GameEngineImpl() {
-        this.controller = new GameControllerImpl();
+        this.model = new WorldImpl();
+        this.mainFrame = new JFrame("SUPER-SONIC");
+        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.mainFrame.setSize(WIDTH, HEIGHT);
+        this.view = new GameViewImpl(mainFrame);
+        this.input = new InputManager();
+        this.mainFrame.addKeyListener(input);
+        this.state = GameState.RUNNING;
     }
 
     @Override
     public void initGame() {
-        this.controller.initGame();
+        this.model.loadWorld(WORLD_FILE_PATH);
+        this.view.initView();
     }
 
-    @Override
     public void mainLoop() {
-        long previousCycleStartTime = System.currentTimeMillis();
         while (true) {
+            switch (state) {
+                case LAUNCHER -> {/*tthis.view.renderMenu*/}
+                case RUNNING -> gameLoop();
+                case GAMEOVER_WON -> {/*this.view.renderEndGameMenu()*/}
+                case GAMEOVER_LOST -> {/*this.view.renderEndGameMenu()*/}
+                default -> { }
+            }
+        }
+    }
+
+    private void gameLoop() {
+        long previousCycleStartTime = System.currentTimeMillis();
+        while (!this.model.isGameOver()) {
             final long currentCycleStartTime = System.currentTimeMillis();
             final long elapsed = currentCycleStartTime - previousCycleStartTime;
             processInput();
@@ -37,21 +71,26 @@ public final class GameEngineImpl implements GameEngine {
             waitForNextFrame(currentCycleStartTime);
             previousCycleStartTime = currentCycleStartTime;
         }
+        if (this.model.isWon()) {
+            this.state = GameState.GAMEOVER_WON;
+        } else {
+            this.state = GameState.GAMEOVER_LOST;
+        }
     }
 
     @Override
     public void processInput() {
-        this.controller.processInput();
+        this.model.setPlayerFlags(input.isRight(), input.isLeft(), input.isJump());
     }
 
     @Override
     public void updateGame(final long elapsed) {
-        this.controller.update(elapsed);
+        this.model.updateGame(elapsed); //TODO: check endgame
     }
 
     @Override
     public void render() {
-        this.controller.render();
+        this.view.renderView(this.model.getGameEntities(), this.model.getPlayer(), this.model.getHud());
     }
 
     @Override
@@ -64,4 +103,14 @@ public final class GameEngineImpl implements GameEngine {
         }
     }
 
+    private enum GameState {
+        
+        LAUNCHER,
+        GAMEOVER_WON,
+        GAMEOVER_LOST,
+        RUNNING
+
+    }
+
 }
+
