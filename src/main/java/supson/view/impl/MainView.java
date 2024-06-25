a238f0a29f1a0673b0c642480f3fd9828df5d52c
Saad El Maliki <saad.elmaliki@studio.unibo.it>
2024-06-25 19:07:35 +0200

    added MainView class

---
+++ src/main/java/supson/view/impl/MainView.java
@@ -0,0 +1,55 @@
+package supson.view.impl;
+
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import supson.model.entity.api.GameEntity;
+import supson.model.entity.impl.moveable.player.Player;
+import supson.model.hud.api.Hud;
+import supson.view.api.EndGameView;
+import supson.view.api.GameView;
+import supson.view.api.MenuView;
+
+public class MainView {
+
+    private static final int WIDTH = 948;
+    private static final int HEIGHT = 720;
+
+    private final JFrame mainFrame;
+    private final MenuView menuView;
+    private final GameView gameView;
+    private final EndGameView endGameView;
+
+    public MainView() {
+        this.mainFrame = new JFrame("SUPER-SONIC");
+        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.mainFrame.setSize(WIDTH, HEIGHT);
+        this.mainFrame.setResizable(false);
+        this.menuView = new MenuViewImpl();
+        this.gameView = new GameViewImpl(this.mainFrame);
+        this.endGameView = new EndGameViewImpl();
+    }
+    
+    public void showMenu(){
+        this.menuView.initView();
+        this.menuView.renderView();
+    }
+
+    public void showGameView() {
+        gameView.initView();
+    }
+
+    public void renderGameView(final List<GameEntity> gameEntitiesList, final Player player, final Hud hud) {
+        this.gameView.renderView(gameEntitiesList, player, hud);
+    }
+
+    public void showEndGame(){
+        endGameView.initView();
+        endGameView.renderView();
+    }
+
+    public void addInputManager(InputManager input){
+        this.mainFrame.addKeyListener(input);
+    }
+}
