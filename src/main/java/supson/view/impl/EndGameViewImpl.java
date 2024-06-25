36c404d65326a18a1f8239ae3e722fe9d12b6484
Saad El Maliki <saad.elmaliki@studio.unibo.it>
2024-06-25 16:45:11 +0200

    added EndGameViewImpl

---
+++ src/main/java/supson/view/impl/EndGameViewImpl.java
@@ -0,0 +1,19 @@
+package supson.view.impl;
+
+import supson.view.api.EndGameView;
+
+public class EndGameViewImpl implements EndGameView {
+
+    @Override
+    public void initView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initView'");
+    }
+
+    @Override
+    public void renderView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'renderView'");
+    }
+
+}
