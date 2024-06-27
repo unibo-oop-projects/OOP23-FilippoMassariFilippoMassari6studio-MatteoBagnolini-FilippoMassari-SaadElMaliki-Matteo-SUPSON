17ad789f0a622d308e6f0762999f9f98be784381
Filippo Massari - filippo.massari6@studio.unibo.it <filippo.massari6@studio.unibo.it>
2024-06-27 15:07:44 +0200

    some stile change for check

--- src/test/java/model/TestWorld.java
+++ src/test/java/model/TestWorld.java
@@ -2,7 +2,10 @@ package model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
@@ -186,7 +189,7 @@ final class TestWorld {
      */
     @Test
     void testAddEnemy() {
-        Enemy enemy = new Enemy(new Pos2dImpl(0,0));
+        Enemy enemy = new Enemy(new Pos2dImpl(0, 0));
         int initialEnemyCount = world.getEnemies().size();
         world.addEnemy(enemy);
         assertEquals(initialEnemyCount + 1, world.getEnemies().size());
