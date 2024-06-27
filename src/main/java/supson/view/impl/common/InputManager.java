80f0bf8ecab5922be124f82535beb27b7bcb7068
Saad El Maliki <saad.elmaliki@studio.unibo.it>
2024-06-27 20:04:14 +0200

    checkstyle fix

--- src/main/java/supson/view/impl/common/InputManager.java
+++ src/main/java/supson/view/impl/common/InputManager.java
@@ -3,17 +3,36 @@ package supson.view.impl.common;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
-public class InputManager implements KeyListener {
+/**
+ * The InputManager class implements KeyListener to manage keyboard input to control player actions.
+ */
+public final class InputManager implements KeyListener {
+
     private boolean left, right, jump;
 
+    /**
+     * Returns the current state of the left arrow key.
+     *
+     * @return true if the left arrow key is pressed, false otherwise
+     */
     public boolean isLeft() {
         return left;
     }
 
+    /**
+     * Returns the current state of the right arrow key.
+     *
+     * @return true if the left arrow key is pressed, false otherwise
+     */
     public boolean isRight() {
         return right;
     }
 
+    /**
+     * Returns the current state of the space key.
+     *
+     * @return true if the space key is pressed, false otherwise
+     */
     public boolean isJump() {
         return jump;
     }
@@ -59,6 +78,5 @@ public class InputManager implements KeyListener {
                 break;
         }
     }
-
 
 }
